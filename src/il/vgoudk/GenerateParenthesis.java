2642f19077f477523c4944e22a918436ca28e023
Victor Goudkov <vgoudkov@iitdgroup.ru>
2022-05-15 22:49:13 +0300

    main GenerateParenthesis

--- src/il/vgoudk/GenerateParenthesis.java
+++ src/il/vgoudk/GenerateParenthesis.java
@@ -1,55 +1,30 @@
 package il.vgoudk;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GenerateParenthesis {
     public List<String> generateParenthesis(int n) {
-        Set<String> results = new HashSet<>();
-        final String baseBlock = getBaseBlock(n);
-        results.add( baseBlock);
-        doAllSwaps(baseBlock, results, baseBlock.length() - 1);
-        return new ArrayList<>( results);
+        char[] data = new char[n*2];
+        data[0] = '(';
+        final ArrayList<String> out = new ArrayList<>();
+        generate(data, 1, 1, out);
+        return out;
     }
 
+    void generate(char[] current, int atPos, int currentBalance, List<String> out) {
 
-    void doAllSwaps(String source, Set<String> accumulator, int distance) {
-        if (distance == 0) return;
-        String changed = source;
-        for (int i = 0; i < source.length() - distance; i++) {
-            doAllSwaps(changed, accumulator, distance - 1);
-            if (source.charAt(i) == source.charAt(i + distance)) continue;
-            changed = change(source, i, distance);
-            if (!isValid(changed.toCharArray())) continue;
-            accumulator.add(changed);
+        if( currentBalance < 0) return;
+
+        if (atPos == current.length) {
+            if (currentBalance == 0) out.add(String.valueOf(current));
+            return;
         }
+        //1-st - (
+        current[atPos] = '(';
+        generate(current, atPos + 1, currentBalance + 1, out);
+        //2-st - )
+        current[atPos] = ')';
+        generate(current, atPos + 1, currentBalance - 1, out);
     }
-
-    String change(String source, int fromIndex, int atDistance) {
-        char[] chars = source.toCharArray();
-        final int left = fromIndex;
-        final int right = fromIndex + atDistance;
-        if (right >= chars.length) return source; //TODO: should not occure...
-        final char tmp = chars[left];
-        chars[left] = chars[right];
-        chars[right] = tmp;
-        return String.valueOf(chars);
-    }
-
-    String getBaseBlock(int n) {
-        return "(".repeat(n) + ")".repeat(n);
-    }
-
-    boolean isValid(char[] data) {
-        int balance = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == '(') balance++;
-            else if (data[i] == ')') balance--;
-            if (balance < 0) return false;
-        }
-        return balance == 0;
-    }
-
 }
